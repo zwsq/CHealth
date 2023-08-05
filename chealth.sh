b4a78b8397af47d6865f157048b8f379d029fe04
ZWSQ <abolemail@gmail.com>
2023-08-05 20:52:32 +0330

    the script is now compatible with POSIX Shell

--- chealth.sh
+++ chealth.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Function to check the health status of a container
 check_health_status() {
@@ -18,33 +18,28 @@ restart_container() {
 log_error() {
     error_message="$1"
     echo "Error: $error_message" >&2
-    logger -p err -t DockerStatus "$error_message"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - DockerStatus: $error_message" | logger -p user.err
 }
-
-# Read container names from the external file
-containers_file="chealth.conf"
-containers=()
-
-if [ -f "$containers_file" ]; then
-    while IFS= read -r line; do
-        containers+=("$line")
-    done < "$containers_file"
-else
-    echo "Error: $containers_file not found. Please create the file and add container names, one per line."
-    exit 1
-fi
 
 # Main loop
 while true; do
-    for container in "${containers[@]}"; do
-        health_status=$(check_health_status "$container")
-        echo "Container: $container, Health Status: $health_status"
+    # Read container names from chealth.conf file
+    containers_file="chealth.conf"
 
-        if [[ "$health_status" != "healthy" ]]; then
-            restart_container "$container"
-            log_error "Container '$container' is unhealthy, attempting to restart it."
-        fi
-    done
+    if [ -f "$containers_file" ]; then
+        while IFS= read -r container; do
+            health_status=$(check_health_status "$container")
+            echo "Container: $container, Health Status: $health_status"
+
+            if [ "$health_status" != "healthy" ]; then
+                restart_container "$container"
+                log_error "Container '$container' is unhealthy and was restarted."
+            fi
+        done < "$containers_file"
+    else
+        echo "Error: $containers_file not found. Please create the file and add container names, one per line."
+        exit 1
+    fi
 
     sleep 30
 done
